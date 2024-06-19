b2b3e8e49638ec7162faea500be08cd417d63aee
CaePolit <caepolit@gmail.com>
2024-06-19 09:13:10 +0200

    Ajoutez des fichiers projet.

---
+++ machine_industrielle/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace machine_industrielle
+{
+    public class BeerEncapsulator
+    {
+        private int _avalaibleBeerVolume = 0;
+        private int _avalaibleBottles = 0;
+        private int _avalaibleCapsules = 0;
+        //public int BeersToDo;
+
+        public BeerEncapsulator(int avalaibleBeerVolume, int avalaibleBottles, int avalaibleCapsules)
+        {
+            _avalaibleBeerVolume = avalaibleBeerVolume;
+            _avalaibleBottles = avalaibleBottles;
+            _avalaibleCapsules = avalaibleCapsules;
+        }
+        public void AddBeer(int additionalVolume) //fonction qui modifi la cantité de biere dans la machine 
+        {
+            _avalaibleBeerVolume += additionalVolume;
+        }
+
+        public void AddBottle(int additionalBottles)
+        {
+            _avalaibleBottles = +additionalBottles;
+        }
+
+        public void AddCaps(int additionalCapsules)
+        {
+            _avalaibleCapsules = +additionalCapsules;
+        }
+
+        public int ProduceEncapsulatedBeerBottles(int beersToDo)
+        {
+            int beerVolume = 33 * beersToDo;
+            int encapsulatedBeer;
+            if (_avalaibleBeerVolume >= beerVolume && ((_avalaibleBottles >= beersToDo) && (_avalaibleCapsules >= beersToDo)))
+            {
+                encapsulatedBeer = beersToDo;
+                //AddBeer(-33 * beersToDo);
+                //AddBottle(-1 * beersToDo);
+                //AddCaps(-1 * beersToDo);
+            }
+            else { encapsulatedBeer = 0; }
+
+
+            return encapsulatedBeer;
+        }
+
+        public void DisplayMessage(int encapsulatedBeer, int beersToDo)
+        {
+            int beerVolume = 33 * beersToDo;
+            if (encapsulatedBeer == 0 && _avalaibleBeerVolume < beerVolume)
+            {
+                int beerPlus = beerVolume - _avalaibleBeerVolume;
+                Console.WriteLine("ATTENTION!Y a pas assez de biere. Remplir: " + beerPlus);
+            }
+            else if (encapsulatedBeer == 0 && _avalaibleBottles < beersToDo)
+            {
+                int bottlesPlus = beersToDo - _avalaibleBottles;
+                Console.WriteLine("ATTENTION!Y a pas assez de biere. Remplir: " + bottlesPlus);
+            }
+            else if (encapsulatedBeer == 0 && _avalaibleCapsules < beersToDo)
+            {
+                int capsulesPlus = beersToDo - _avalaibleCapsules;
+                Console.WriteLine("ATTENTION!Y a pas assez de biere. Remplir: " + capsulesPlus);
+            }
+            else { Console.WriteLine("You have made " + beersToDo + " bottles!"); }
+        }
+    }
+}
---
+++ machine_industrielle/Program.cs
@@ -0,0 +1,31 @@
+﻿using machine_industrielle;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter the volume of available centiliters (entier):");
+        string entry1 = Console.ReadLine();
+        int avalaibleBeerVolume = Convert.ToInt32(entry1);
+        Console.WriteLine("enter the number of available bottles (entier):");
+        string entry2 = Console.ReadLine();
+        int avalaibleBottles = Convert.ToInt32(entry2);
+        Console.WriteLine("enter the number of available caps (entier):");
+        string entry3 = Console.ReadLine();
+        int avalaibleCapsules = Convert.ToInt32(entry3);
+        BeerEncapsulator beerEncapsulator = new BeerEncapsulator(avalaibleBeerVolume, avalaibleBottles, avalaibleCapsules);
+
+        Console.WriteLine("how many beers you want to produce?: ");
+        string entry4 = Console.ReadLine();
+        int beersToDo = Convert.ToInt32(entry3);
+        int beers = beerEncapsulator.ProduceEncapsulatedBeerBottles(beersToDo);
+        Console.WriteLine(beers);
+        beerEncapsulator.DisplayMessage(beers, beersToDo);
+
+
+
+
+
+    }
+}
